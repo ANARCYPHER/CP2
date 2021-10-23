792c6c39f6ae23798f8e3def024c6b7b7732ad8c
AllSafeCybercurity <skull32273227@gmail.com>
2021-10-23 02:57:07 -0700

    '73'

---
+++ 73b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int  main()
+
+{
+
+   int  num1, num2;
+
+   printf("Enter num1:");
+
+   scanf("%d", & amp; num1); // reading num1 using %d
+
+   printf("Enter num2:");
+
+   scanf("%i", & amp; num2); //reading num2 using %i
+
+   printf("num1: %d \n", num1);
+
+   printf("num2: %d \n", num2);
+
+   return  0;
+
+}
